14da38061aadaec6e80908dd40f2b9f134fa446e
joaozinho <joaozinho@gmail.com>
2024-05-06 08:28:07 -0300

    pokemon

---
+++ src/listagem/lista_pokekmon.jsx
@@ -0,0 +1,16 @@
+import React, (useState, useeffect) from 'react'
+
+const Listagem = () => (
+    <div>
+        <ul>
+            <li>Pokeón 1</li>
+            <li>Pokeón 2</li>
+            <li>Pokeón 3</li>
+            <li>Pokeón 4</li>
+            <li>Pokeón 5</li>
+            <li>Pokeón 6</li>
+        </ul>
+    </div>
+);
+
+export default Listagem
